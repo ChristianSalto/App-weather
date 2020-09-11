03354c06e7590b946459e93118c107a31ef4d0b5
christian <romeosaltovk@gmail.com>
2020-09-11 15:57:38 +0200

    react-router-dom

---
+++ src/components/ForecastChart/ForecastChart.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ForecastChart from './ForecastChart';
+import { render } from '@testing-library/react';
+
+const data = [
+  {
+    dayHour: 'Jue 18',
+    min: 14,
+    max: 22,
+  },
+  {
+    dayHour: 'Vie 28',
+    min: 12,
+    max: 24,
+  },
+  {
+    dayHour: 'Sab 1',
+    min: 15,
+    max: 19,
+  },
+  {
+    dayHour: 'Lun 9',
+    min: 5,
+    max: 11,
+  },
+  {
+    dayHour: 'Dom 12',
+    min: 20,
+    max: 27,
+  },
+  {
+    dayHour: 'Mie 26',
+    min: 11,
+    max: 18,
+  },
+];
+
+test('ForecastChart render', async () => {
+  const { getByText } = render(<ForecastChart data={data} />);
+
+  const weekDay = await getByText('Sab 1');
+
+  expect(weekDay).toBeInTheDocument();
+});
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WelcomePage = (props) => {
+  return (
+    <div>
+      <h1>WelcomePage !!!!!!</h1>
+    </div>
+  );
+};
+
+WelcomePage.propTypes = {};
+
+export default WelcomePage;
